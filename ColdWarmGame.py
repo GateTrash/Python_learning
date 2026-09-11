d7a3cccd9402b9cf678d5f9e61c446bc254b190d
Guest <294281718+GateTrash@users.noreply.github.com>
2026-09-11 18:50:09 +0300

    git #	modified:   README.md

--- ColdWarmGame.py
+++ ColdWarmGame.py
@@ -4,16 +4,19 @@ finish = int(input("Enter the number on finish "))
 rnd = random.randint(start,finish)
 chosen = int(input(f"a random number between {start} and {finish} is chosen. Can you guess it?))"))
 while chosen != rnd:
-    if chosen - rnd < 5:
+    if abs(chosen - rnd) < 5:
         print("Very hot!")
-    elif chosen - rnd < 10:
+    elif abs(chosen - rnd)  < 10:
         print("Hot!")
-    elif chosen - rnd < 20:
+    elif abs(chosen - rnd) < 20:
         print("Warm")
-    elif chosen - rnd < 30:
+    elif abs(chosen - rnd) < 30:
         print("Cold")
-    elif chosen - rnd < 50:
+    elif abs(chosen - rnd) < 50:
         print("So cold")  
-    elif chosen - rnd < 70:
-        print("Very cold!") 
+    elif abs(chosen - rnd)  < 70:
+        print("Very cold!")
+    else:
+        print("Forest!!!")
     chosen = int(input("Your answer:"))
+print(f"Yes {rnd}. You right!")
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Python_learning
 This is my way in Python developt. I am a junior Python developer
+i made several programs:
+1. Age registration(if age more 18 access granted. Else access denied)
+2. Simple calculator(support operation +,-,*,/)
+3. Minigame "guess the number" cold-hot(interactive shooce the number at {} to {}.)
+4. Program for find even or odd numbers
+5. Grade calculator (A>90; B>70; C>50; S<50)
+6. file for MOOS course tasks
+7. Program find max number and print
+8. Miniatm; considers whether it is possible to make a spending
+9. Multiplication table entered number 
+10. Find classification entered number
+11. Program for print bumber at 1 to 100
+12. Program for convert temperature (F to C or C to F)
